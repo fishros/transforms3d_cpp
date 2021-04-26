ccbaba3718632e63a73a2ac4f4a8997a8a5a89c2
sangxin <2876424407@qq.com>
2021-04-26 22:08:51 +0800

    add test data

--- main.cpp
+++ main.cpp
@@ -9,9 +9,10 @@ using namespace Eigen;
 int main()
 {
     Matrix4d Tbg = TransForms::ComposeEuler(3.000006, -279.999992, 375, 180, 0.000001, -135);
-    Matrix4d Tcw  = TransForms::ComposeEuler(-2.48,15.66,300,0,0,0);
-    Matrix4d Tbw  = TransForms::ComposeEuler(3,-343,163.6,180,0,-135);
-    Matrix4d Tgc = Tbg.inverse()*Tbw*Tcw.inverse();
+    Matrix4d Tcw  = TransForms::ComposeEuler(-2.48,15.66,300,0,0,45);
+    Matrix4d Tbw  = TransForms::ComposeEuler(-0.663,-0.193,-0.231,-180,0,140);
+    /* Tbw=Tbg*Tgc*Tcw*/
+    Matrix4d Tgc = Tbg.inverse()*Tbw*Tcw.inverse(); 
     // cout<<"------------------Tgc1---------------"<<endl;
     // cout<<Tbg.inverse()*Tbw*Tcw.inverse()<<endl;
     cout<<TransForms::H2EulerAngle(Tgc);
